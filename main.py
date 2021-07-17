b32079743a879dca0f468999f2be1f8538af006a
PeisenWong <wongpeisem@gmail.com>
2021-07-17 16:11:57 +0800

    Enabled spaceship to move freely

--- main.py
+++ main.py
@@ -15,22 +15,41 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("rocket.png")
 playerX = 370
 playerY = 480
+playerX_change = 0
+playerY_change = 0
 
 # Placing the spaceship in screen
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 # Game loop
 running = True
 while running:
 # RGB = red, green, blue  mixing of colours, 255 is max
     screen.fill((255, 255, 255))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # If typed the 'red cross button'
             running = False
 
-    player()            
+        # Check whether any key is pressed
+        if event.type  == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.5
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.5
+            if event.key == pygame.K_UP:
+                playerY_change = -0.5
+            if event.key == pygame.K_DOWN:
+                playerY_change = 0.5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or pygame.K_RIGHT or pygame.K_UP or pygame.K_DOWN:
+                playerX_change = 0
+                playerY_change = 0
+
+    playerX += playerX_change
+    playerY += playerY_change
+    player(playerX, playerY)            
 
     # Update the screen
     pygame.display.update() 
